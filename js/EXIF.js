b9b9f2b17920d2c41853311b6766937ccb5c57bc
Lilpha <sungho2558@naver.com>
2022-07-18 16:53:42 +0900

    이미지 hidden상태로 추가 후 거기서 GPS따옴

--- js/EXIF.js
+++ js/EXIF.js
@@ -6,23 +6,36 @@ function getExif() {
     EXIF.getData(img2, function() {
         var allMetaData = EXIF.getAllTags(this);
         var allMetaDataSpan = document.getElementById("allMetaDataSpan");
-        console.log(allMetaDataSpan); //span에 텍스트상태
-        console.log(allMetaData)//object상태
+        //console.log(allMetaDataSpan); //span에 텍스트상태
+        //console.log(allMetaData)//object상태
         console.log(Object.entries(allMetaData)); //array형태
         const ExifArray = Object.entries(allMetaData) //array형태
-        const GpsLat = ExifArray[41]; //array에서 41번째 == GpsLatitude
-        const GpsLong = ExifArray[43]; //array에서 43번째 == GpsLongitude
-        console.log(GpsLat); //["GPSLatitude", Array] 의 형태로나옴 아직 array형태 
+        
+        console.log(ExifArray.find(Make));
+        //삼성, 아이폰 나누는 조건부 서식
+        const model = ExifArray[3];
+        console.log(model);        
+        //사진의 촬영기기가 아이폰 일때
+        // const GpsLat = ExifArray[41]; //array에서 41번째 == GpsLatitude
+        // const GpsLong = ExifArray[43]; //array에서 43번째 == GpsLongitude
+        
+        //촬영기기가 삼성일때
+        const GpsLat = ExifArray[22];
+        const GpsLong =ExifArray[26];
+
         const StringLat = JSON.stringify(GpsLat); //stringfy로 string형태로 바꿈 
         const StringLong = JSON.stringify(GpsLong);
-        console.log(StringLat);//["GPSLatitude",[37,32,48.2]] string형태
-        console.log(JSON.parse(StringLat)[1]);//위 string을 parsing해서 1번째 원소 꺼냄 == [37, 32, 48.2] [123, 3, 55.7]
         const realLat = JSON.parse(StringLat)[1];
         const realLong = JSON.parse(StringLong)[1];
-        console.log(realLat);//[37, 32, 48.2]
-        console.log(realLong);//[127, 3, 55.9]
+       
+        
+        // console.log(GpsLat); //["GPSLatitude", Array] 의 형태로나옴 아직 array형태 
+        // console.log(StringLat);//["GPSLatitude",[37,32,48.2]] string형태
+        // console.log(JSON.parse(StringLat)[1]);//위 string을 parsing해서 1번째 원소 꺼냄 == [37, 32, 48.2] [123, 3, 55.7]
+        // console.log(realLat);//[37, 32, 48.2]
+        // console.log(realLong);//[127, 3, 55.9]
         let GpsLoaction = realLat+" "+realLong;
-        console.log(GpsLoaction);//37,32,48.2 127,3,55.9
+        // console.log(GpsLoaction);//37,32,48.2 127,3,55.9
         GpsLoaction = GpsLoaction.replace(/\,/g,' ');
         console.log(GpsLoaction);//37 32 48.2 127 3 55.9
 
--- js/background.js
+++ js/background.js
@@ -3,4 +3,9 @@ const images= ["0.jpg","1.jpg","2.jpg"];
 const chooseImage = images[Math.floor(Math.random() * images.length)]
 document.body.style.backgroundImage = `url("image/${chooseImage}")`;
 
-console.log(document.getElementById('img0').src);
+var img = document.createElement("img");
+img.src = `image/${chooseImage}`;
+img.classList.add("hidden");
+img.id = "img2";
+document.body.appendChild(img);
+
